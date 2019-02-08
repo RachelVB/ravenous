2f4093d119d619a634dd2ff53100c2e8793bb1ad
RachelVB <vanbuskirk2014@hotmail.com>
2019-02-08 08:11:58 -0800

    finished business comp and started searchbar comp

--- ravenousproject/src/components/BusinessList/BusinessList.js
+++ ravenousproject/src/components/BusinessList/BusinessList.js
@@ -4,6 +4,18 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component() {
   render() {
-    
+    return (
+      <div className="BusinessList">
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+      </div>
+    )
   }
 }
+
+/* After creating a component, we must always export it. */
+export default BusinessList;
---
+++ ravenousproject/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component() {
+  renderSortByOptions() {
+    /* This Object.keys return method, allows us to start our iteration by accessing the keys in our object.
+    We complete the iteration by using the .map() methond at the end. */
+    return Object.keys(sortByOptions).map(sortByOption => {
+      /* This let variable allows us to store the results in this variable when we cann the keys in our object. */
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return (
+      <li key={sortByOptionValue}>{sortByOption}</li>
+        )
+    });
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+      .renderSortByOptions()
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Let's Go</a>
+  </div>
+  </div>
+    )
+  }
+}
+
+export default SearchBar;
